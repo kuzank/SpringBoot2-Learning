7ae96d19fff0ab5bf0f7d533655d974d29a2853a
kuzank <754109648@qq.com>
2020-03-10 00:17:13 +0800

    2-0-1 搭建 SpringBoot 2 Api工程

---
+++ chapter2-0-1/src/test/java/com/kuzank/springboot2/Chapter201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzank.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
